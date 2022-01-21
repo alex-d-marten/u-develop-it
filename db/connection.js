ab1f781e2e8d7999bd538dbca8356aec44311596
amarten <alexdmarten@gmail.com>
2022-01-20 22:08:12 -0800

    remake repo

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// Connect to DATABASE
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+
+
+module.exports = db;
